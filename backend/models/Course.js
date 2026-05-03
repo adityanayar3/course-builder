d027bbb0cd3b2a864d12a240d28fe80d718a93ec
Aditya Nayar <aditya.nayar@juspay.in>
2026-05-03 19:05:53 +0530

    feat: define course-module-lesson schema

---
+++ backend/models/Course.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const courseSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: String,
+    creator: {
+      type: String,
+      required: true,
+    },
+    modules: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Module",
+      },
+    ],
+    tags: [
+      {
+        type: String,
+        trim: true,
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Course", courseSchema);
